da530ae44a1a463921001f79593236f428bed586
Anchel17 <80929766+Anchel17@users.noreply.github.com>
2021-12-08 22:01:48 -0300

    Update CanalTexto.cpp

--- src/CanalTexto.cpp
+++ src/CanalTexto.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-CanalTexto::CanalTexto(unsigned int id, std::string nome, Usuario* dono){
+CanalTexto::CanalTexto(int id, std::string nome, Usuario* dono){
     this -> ch_id = id;
     this -> ch_nome = nome;
     this -> ch_dono = dono;
@@ -16,10 +16,29 @@ unsigned int CanalTexto::getCh_Id(){
     return this -> ch_id;
 }
 
-std::string CanalTexto::getCh_Nome(){
+string CanalTexto::getCh_Nome(){
     return this -> ch_nome;
 }
 
 Usuario* CanalTexto::getCh_Dono(){
     return this -> ch_dono;
+}
+
+void CanalTexto::setCh_msg(Mensagem msg){
+    this -> ch_mensagens.push_back(msg);
+}
+
+string CanalTexto::getCh_msg(vector<Usuario*> &user){
+    string retorno = "";
+
+
+    for(auto itMsg = ch_mensagens.begin(); itMsg != ch_mensagens.end(); itMsg++){
+        for(Usuario* u : user){
+            if(u -> getNome() == itMsg -> getMsg_EnviadaPor() -> getNome()){
+                retorno += u -> getNome() + "<" + itMsg -> getMsg_DataHora() + ">: "+   itMsg -> getMsg_Conteudo() + "\n";
+            }
+        }
+    }
+
+    return retorno;
 }
