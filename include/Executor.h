2af775b1570dfc734ffc26db319225b4a38baa21
Anchel17 <80929766+Anchel17@users.noreply.github.com>
2021-12-13 17:28:53 -0300

    Update Executor.h

--- include/Executor.h
+++ include/Executor.h
@@ -1,24 +1,75 @@
-#ifndef EXECUTOR_H
-#define EXECUTOR_H
-#include "Sistema.h"
-#include <istream>
-#include <ostream>
-#include <sstream>
+#ifndef CANALTEXTO_H
+#define CANALTEXTO_H
 
-using namespace std;
+#include <vector>
 
-class Executor {
-  private:
-  Sistema * sistema;
-  stringstream ss;
-  bool sair = false;
+#include "../include/Usuario.h"
+#include "../include/Mensagem.h"
 
-  public:
-  Executor(Sistema &sistema);
+class CanalTexto{
 
-  void iniciar(istream &in, ostream &out);
+    private:
 
-  string processarLinha(string linha);
+        unsigned int ch_id;     //<!Id do canal
+
+        std::string ch_nome;    //<!Nome do canal
+
+        Usuario* ch_dono;       //<!Referência ao dono do canal
+
+        std::vector<Mensagem> ch_mensagens;  //<!Vector de mensagens enviadas no canal
+
+    public:
+
+    /**
+     * Construtor da classe CanalTexto
+     * @param int id do canal
+     * @param string nome do canal
+     * @param Usuario* referência ao dono do canal
+     */
+    CanalTexto(int id, std::string nome, Usuario* dono);
+
+    /**
+     * Método Get para id do canal
+     * @return id do canal
+     */
+    unsigned int getCh_Id();
+
+    /**
+     * Método Get para o nome do canal
+     * @return Nome do canal
+     */
+    std::string getCh_Nome();
+
+    /**
+     * Método Get para o dono do canal
+     * @return referência ao dono do canal
+     */
+    Usuario* getCh_Dono();
+
+    /**
+     * Método para guardar uma mensagem
+     * @param Mensagem contendo as informações da mensagem
+     */
+    void setCh_msg(Mensagem msg);
+
+    /**
+     * Método para retornar as mensagens de um canal
+     * @param Usuario* Referência só para procurar o dono da mensagem
+     * @return string contendo mensagens enviadas de um canal
+     */
+    std::string getCh_msg(std::vector<Usuario*> &user);
+    
+    /**
+     * Método para verificar se um usuário é dono de alguma mensagem em canais
+     * @param Usuario* referência a um usuário
+     * @return true caso o usuário seja dono de alguma mensagem, false caso contrário
+     */
+    bool verCh_msgDono(Usuario* &user);
+
+    /**
+     * Método para tornar o usuário dono do canal a ser deletado anônimo
+     */
+    void setCH_donoAnonimo();
 };
 
 #endif
